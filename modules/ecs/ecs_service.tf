1ecdeefa51639ff277cdb319580e700ab6154e2c
Aryan Dudhat <aryandudhat84@gmail.com>
2026-09-07 12:03:23 +0530

    INFRA:other minor error fixes

--- modules/ecs/ecs_service.tf
+++ modules/ecs/ecs_service.tf
@@ -139,7 +139,7 @@ resource "aws_ecs_service" "this" {
 
   propagate_tags = "SERVICE"
 
-  availability_zone_rebalancing = "enabled"
+    availability_zone_rebalancing = "ENABLED"
 
   # ----------------------------------------------------------
   # LOAD BALANCER HEALTH
